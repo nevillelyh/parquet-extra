0e308f3f04bbd4d849adf6f43fab30147729a997
Neville Li <neville@spotify.com>
2015-09-01 11:18:48 -0400

    update scala version

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object BuildSettings {
     version            := "0.1.6",
 
     scalaVersion       := "2.10.5",
-    crossScalaVersions := Seq("2.10.5", "2.11.6"),
+    crossScalaVersions := Seq("2.10.5", "2.11.7"),
     scalacOptions      ++= Seq("-target:jvm-1.7", "-deprecation", "-feature", "-unchecked"),
     javacOptions       ++= Seq("-source", "1.7", "-target", "1.7"),
 
